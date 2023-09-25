3c310788af03008d6203b8e3afec407d16e8842a
Hayespace <ben.hayes@hotmail.com>
2023-09-25 14:07:35 +0000

    Add categories dropdown

--- app.py
+++ app.py
@@ -100,7 +100,8 @@ def logout():
 
 @app.route("/add_task")
 def add_task():
-    return render_template("add_task.html")
+    catagories = mongo.db.catagories.find().sort("category_name", 1)
+    return render_template("add_task.html", catagories=catagories)
 
 
 if __name__ == "__main__":
--- static/js/script.js
+++ static/js/script.js
@@ -1,13 +1,14 @@
-$(document).ready(function(){
-    $('.sidenav').sidenav({edge: "right"});
-    $('.collapsible').collapsible();
-    $('.tooltipped').tooltip();
-    $('.datepicker').datepicker({
+$(document).ready(function () {
+  $(".sidenav").sidenav({edge: "right"});
+  $(".collapsible").collapsible();
+  $(".tooltipped").tooltip();
+  $("select").formSelect();
+  $(".datepicker").datepicker({
       format: "dd mmmm, yyyy",
       yearRange: 3,
       showClearBtn: true,
       i18n: {
-        done: "Select"
+          done: "Select"
       }
-    });
   });
+});
